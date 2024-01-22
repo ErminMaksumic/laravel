f40dc3483fe754b33dd23c0819786103e68c590f
Ermin Maksumić <56846473+ErminMaksumic@users.noreply.github.com>
2024-01-22 22:22:11 +0100

    Repository test (#10)
    * test
    
    * test

---
+++ react/src/app/homepage/page.tsx
@@ -0,0 +1,9 @@
+import { CustomDataGrid } from '@/app/components/CustomDataGrid';
+
+export default function Home() {
+  return (
+    <div style={{ padding: '5%' }}>
+      <CustomDataGrid></CustomDataGrid>
+    </div>
+  );
+}
---
+++ react/src/lib/columns.tsx
@@ -0,0 +1,5 @@
+export const columns = [
+  { field: 'id', headerName: 'ID', width: 70 },
+  { field: 'name', headerName: 'Name', width: 500 },
+  { field: 'price', headerName: 'Price', width: 500 },
+];
