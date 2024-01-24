1e0e62454849a971b9ebbb161baf51c992ba4b8b
ErminMaksumic <maksumicermin@gmail.com>
2024-01-24 11:51:32 +0100

    formatting

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -22,13 +22,14 @@ use Illuminate\Support\Facades\Route;
 Route::post('login', [AuthController::class, 'login']);
 Route::post('logout', [AuthController::class, 'logout'])->middleware("auth:sanctum");
 
-// Resource route
+// resource route
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) { return $request->user(); });
 Route::get('rentalCar/prices', [RentalCarController::class, 'getAllPrices']);
 Route::apiResource('rentalCar', RentalCarController::class);
 Route::apiResource('reservation', ReservationController::class);
 Route::apiResource('payment', PaymentController::class);
 
+// state machine
 Route::get('/payment/{id}/allowedActions', [PaymentController::class, 'allowedActions'])->name('payment.allowedActions');
 Route::put('/payment/{id}/paymentProcess', [PaymentController::class, 'paymentProcess'])->name('payment.paymentProcess');
 Route::put('/payment/{id}/paymentApprove', [PaymentController::class, 'paymentApprove'])->name('payment.paymentApprove');
